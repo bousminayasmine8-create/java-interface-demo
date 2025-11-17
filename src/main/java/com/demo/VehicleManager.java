dd1bb64f9a3bd5915b2e68dc4944e631d57be970
Yasmine Bousmina <bousminayasmine8@gmail.com>
2025-11-17 21:29:26 +0100

    Ajout de tous les fichiers Java et du pom.xml

---
+++ src/main/java/com/demo/VehicleManager.java
@@ -0,0 +1,38 @@
+package com.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class VehicleManager {
+    private List<Vehicle> vehicles = new ArrayList<>();
+
+    public void addVehicle(Vehicle vehicle) {
+        vehicles.add(vehicle);
+    }
+
+    public void startAll() {
+        for (Vehicle v : vehicles) {
+            v.start();
+        }
+    }
+
+    public void stopAll() {
+        for (Vehicle v : vehicles) {
+            v.stop();
+        }
+    }
+
+    public int getTotalVehicles() {
+        return vehicles.size();
+    }
+
+    public static void main(String[] args) {
+        VehicleManager manager = new VehicleManager();
+        manager.addVehicle(new Car("Toyota"));
+        manager.addVehicle(new Motorcycle("Yamaha"));
+
+        System.out.println("Total véhicules: " + manager.getTotalVehicles());
+        manager.startAll();
+        manager.stopAll();
+    }
+}
