5a57a50b0960e53e99e5c11bec221fa32cb064e1
Yasmine Bousmina <bousminayasmine8@gmail.com>
2025-11-19 22:24:01 +0100

    Initial commit - projet Java + web

---
+++ src/main/java/com/demo/Motorcycle.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+public class Motorcycle implements Vehicle {
+    private String model;
+
+    public Motorcycle(String model) { this.model = model; }
+
+    @Override
+    public void start() { System.out.println("La moto " + model + " démarre"); }
+    @Override
+    public void stop() { System.out.println("La moto " + model + " s'arrête"); }
+    @Override
+    public String getType() { return "Motorcycle"; }
+}
---
+++ src/main/java/com/demo/Vehicle.java
@@ -0,0 +1,7 @@
+package com.demo;
+
+public interface Vehicle {
+    void start();
+    void stop();
+    String getType();
+}
