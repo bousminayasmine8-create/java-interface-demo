af37d0a2349361774206d2a2b5d3765230cc6245
Yasmine Bousmina <bousminayasmine8@gmail.com>
2025-11-21 11:00:08 +0100

    Correction Main.java

--- src/main/java/com/demo/Main.java
+++ src/main/java/com/demo/Main.java
@@ -1,3 +1,5 @@
+package com.demo;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Jenkins!");
--- src/main/java/com/demo/servlets/LoginServlet.java
+++ src/main/java/com/demo/servlets/LoginServlet.java
@@ -7,21 +7,16 @@ import java.io.IOException;
 public class LoginServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // Redirige vers la page login.html si quelqu’un tape /login dans le navigateur
-        response.sendRedirect("login.html");
-    }
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
 
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
 
-        // Exemple simple de vérification
         if ("admin".equals(username) && "1234".equals(password)) {
-            response.sendRedirect("dashboard.html"); // succès → dashboard
+            response.sendRedirect("dashboard.html");
         } else {
-            response.sendRedirect("login.html?error=1"); // échec → login
+            response.sendRedirect("index.html?error=1");
         }
     }
 }
